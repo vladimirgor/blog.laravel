609ace282773e082d760ef2245ab1ec23cebc7d4
vladimirgor <vladimirgor2011@yandex.ru>
2017-01-24 17:58:10 +0500

    all messages and comments in english

--- app/Http/Controllers/AdvancedReg.php
+++ app/Http/Controllers/AdvancedReg.php
@@ -17,25 +17,26 @@ class AdvancedReg extends Controller
     public function register(Request $request)
     {
         $this->validate($request, [
-            'name'  => 'required|max:150',
-            'login' => 'required|max:150',
-            'email' => 'required|max:250|email',
-            'password' => 'required|confirmed|min:6',
+            'name'  => 'required|max:150', // entered name
+            'login' => 'required|max:150', // entered login
+            'email' => 'required|max:250|email', // entered email
+            'password' => 'required|confirmed|min:6', // entered password
         ]);
-        $user=User::where('email','=',$request->input('email'))->first(); //делаем выборку из базы по введенному email
+        
+        $user=User::where('email','=',$request->input('email'))->first(); //selection from the user table  according to the entered email
 
-        if(!empty($user->email))
+        if(!empty($user->email)) // if exist user with the entered email
         {
-            if($user->status==0)      // если email существует и его статус 0, то предлагаем повторно отправить письмо
+            if($user->status==0)      // if email exists and the status 0, the offer to re-send a confirmation letter
             {
-                return 'Такой email уже зарегестрирован, но не подтвержден.
-                        роверьте почту или <a href="repeat_confirm">запросите</a>
-                        повторное подтверждение email';
+                return 'This email is already registered, but not confirmed.
+                        Please, check your email or <a href="repeat_confirm">ask for</a>
+                        re-confirmation email.';
 
             }
-            else  //если статус не равен 0, то равен 1, следовательно email уже подтвержден
+            else  //If status is not equal to 0, 1 is thus already been verified email
             {
-                return "Пользователь с таким email уже зарегестрирован. Забыли пароль?";
+                return "A person with the entered email is already registered.";
             }
         }
 
@@ -48,7 +49,7 @@ class AdvancedReg extends Controller
         ]);
 
         if($user)
-        {
+        { // if the user insert is successful
             $email=$user->email;  // user email
             $token=str_random(32); //random string
             $model=new ConfirmUsers; //new model instance
@@ -62,22 +63,21 @@ class AdvancedReg extends Controller
                 $u->subject('Confirm registration');
             });
 
-            return back()->with('message','All right. Email confirmation left only.Check your email to confirm your email address.
-            ');
+            return back()->with('message','Letter with the confirmation link sent to your email.
+            Please,check your email.');
         }
-        else {
-            return back()->with('message','Something went wrong.Please try again later.');
+        else {//if the user insert is not successful
+            return back()->with('message','Something went wrong.Please,try again later.');
         }
     }
     public function confirm($token)
     {
         $model=ConfirmUsers::where('token','=',$token)->firstOrFail(); //record with link token select, 404 error if missing
-        $user=User::where('email','=',$model->email)->first(); //record with link token select
+        $user=User::where('email','=',$model->email)->first(); //record with email  select
         $user->status=1; // status change to 1
         $user->save();  // save changing
-        $model->delete(); //Удаляем запись из confirm_users record delete
+        $model->delete(); //confirm_users record delete
         return "Registration is finished successfully. Congratulations!";
-        //return redirect('/');
     }
 
     public function getRepeat()
@@ -85,31 +85,31 @@ class AdvancedReg extends Controller
     {
 
         return view('auth.repeat');
-//возвращаем вид с формой для ввода email
+//return the view  with  the form to enter email
     }
     public function postRepeat(Request $request)
     {
-        $user=User::where('email','=',$request->input('email'))->first(); //делаем выборку из таблицы users по указанному email
+        $user=User::where('email','=',$request->input('email'))->first(); //record with entered email select from user table
 
-        if(!empty($user->email)) // проверяем, что email существует
+        if(!empty($user->email)) // is email existing
         {
             if($user->status==0 )
             {
-                $user->touch(); // это метод, который обновляет поле updated_at на текущее время.
-                $confirm=ConfirmUsers::where('email','=',$request->input('email'))->first(); //делаем выборку из таблицы confrim_users
-                $confirm->touch(); // тоже обновляем поле updated_at
+                $user->touch(); //updating the updated_at field at the current time.
+                $confirm=ConfirmUsers::where('email','=',$request->input('email'))->first(); //record with entered email select from confrim_users table
+                $confirm->touch(); // updating the updated_at field at the current time.
 
-                Mail::send('email/confirm',['token'=>$confirm->token], function($u) use ($user) //отправляем письмо пользователю
+                Mail::send('email/confirm',['token'=>$confirm->token], function($u) use ($user) //send confirmation letter to user
                 {
                     $u->to($user->email);
-                    $u->subject('Подтверждение email');
+                    $u->subject('Email confirmation');
                 });
-                return back()->with('message','Письмо для активации успешно выслано на указанный адрес'); //возвращаем пользователя обратно с сообщением, что письмо отправленно
+                return back()->with('message','Letter to confirm successfully sent to the entered address.'); //return with  that the message was sent
             }
             else {
-                return "Такой email уже подтвержден";}
+                return "This email has been confirmed already.";}
         }
-        else { return "Нет пользователя с таким email";}
+        else { return "No user with this email.";}
 
     }
 
