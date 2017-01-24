72d78c635108fb8de94d5ac1ca65dd687a1b4f0b
vladimirgor <vladimirgor2011@yandex.ru>
2017-01-24 17:53:44 +0500

    changed for bootstrap

--- resources/views/auth/repeat.blade.php
+++ resources/views/auth/repeat.blade.php
@@ -1,10 +1,43 @@
-<form method="POST">
-    <label for="email">Your email</label>
-    <input type="email" name="email"/>
-    <input type="hidden" name="_token" value="{{csrf_token()}}"/>
-    <br>
-    <input type="submit" value="Request"/>
-</form>
-@if(Session::has('message'))
-    {!!Session::get('message')!!}
-@endif
+@extends('layouts.mes')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Confirmation repeat</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST">
+                            {{ csrf_field() }}
+                            <!-- email -->
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-btn fa-user"></i> Send
+                                    </button>
+                                </div>
+                            </div>
+                            @if(Session::has('message'))
+                                <strong>{!!Session::get('message')!!}</strong>
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
