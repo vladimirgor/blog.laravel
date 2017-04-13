bbf7aa55af6d009c9527b1584d5372c333435009
vladimirgor <vladimirgor2011@yandex.ru>
2017-04-13 18:21:26 +0500

    shadow buttons implemented

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -56,7 +56,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="button btn-primary">
                                     <i class="fa fa-btn fa-refresh"></i> Reset Password
                                 </button>
                             </div>
