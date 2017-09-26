d13ac7f88fe0e74e55a42434d20b2b186a1274b2
vladimirgor <vladimirgor2011@yandex.ru>
2017-09-26 14:49:19 +0500

    showComments function added

--- app/Http/Controllers/ModerController.php
+++ app/Http/Controllers/ModerController.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Comment;
 
 class ModerController extends Controller
 {
-    //
+    public function showComments(){
+        $this->authorize('moderationComments');
+        $comments = Comment::select(['id','article_id','user_id','comment',
+           'date','status'])->where('status',0)->orderBy('id','desc')
+            ->paginate(PER_PAGE_COMMENT)
+        ;
+        return view('show-comments')->with(['comments'=>$comments]);
+    }
 }
