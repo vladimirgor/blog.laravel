a6a14790e42e2e936cd50415eae70c04c426b598
vladimirgor <vladimirgor2011@yandex.ru>
2017-09-28 17:35:21 +0500

    comment_mod table migration added

---
+++ database/migrations/2017_09_28_153234_comment_mod.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CommentMod extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_mod', function (Blueprint $table) {
+            $table->integer('id');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comment_mod');
+    }
+}
