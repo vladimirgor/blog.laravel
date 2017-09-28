9f65f5ca2741c7a0f4e3daf840b56cc339120e77
vladimirgor <vladimirgor2011@yandex.ru>
2017-09-28 23:06:49 +0500

    coordinate class names for confirm_users and comment_mod table migrations

--- database/migrations/2016_12_14_081110_create_confirm_users_table.php
+++ database/migrations/2016_12_14_081110_create_confirm_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ConfirmUsers extends Migration
+class CreateConfirmUsersTable extends Migration
 
 {
     /**
--- database/migrations/2017_09_28_153234_create_comment_mod_table.php
+++ database/migrations/2017_09_28_153234_create_comment_mod_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CommentMod extends Migration
+class CreateCommentModTable extends Migration
 {
     /**
      * Run the migrations.
