f1feb7b9dfae10737ed8aa4036a0fb306c941be9
vladimirgor <vladimirgor2011@yandex.ru>
2017-09-28 18:28:35 +0500

    comments added to script and all() implemented

--- app/Http/Controllers/ModerController.php
+++ app/Http/Controllers/ModerController.php
@@ -12,16 +12,19 @@ class ModerController extends Controller
 {
     public function showComments(){
         $this->authorize('moderationComments');
+        // cleaning Comments_mod table
         $comments_mod = Comment_mod::all();
         if ( !empty($comments_mod)) {
             foreach ( $comments_mod  as $comment_mod ) {
                 $comment_mod->delete();
             }
         }
+        //getting 0 status comments
         $comments = Comment::select(['id','article_id','user_id','comment',
            'date','status'])->where('status',0)->orderBy('id','desc')->get();
-        if ( !empty($comments) )
-        {
+                if ( !empty($comments) )
+        //saving 0 status information
+                {
             foreach ($comments as $comment ){
                 $comment_mod = new Comment_mod;
                 $comment_mod->id = $comment->id;
@@ -29,24 +32,30 @@ class ModerController extends Controller
                 $comment_mod->save();
             }
         }
+        //show form to confirm comments
         return view('show-comments')->with(['comments'=>$comments]);
     }
 
     public function confirmationComments(){
         $this->authorize('moderationComments');
+        //Getting confirmed comments information
         foreach($_POST as $article_id => $id){
             if ( $article_id != '_token' ) {
+                // add 1 to comments  count in Article table
                 $article = Article::where('id',$article_id)->first();
                 $article->comment++;
                 $article->save();
+                //change comment status in Comment table
                 $comment = Comment::where('id',$id)->first();
                 $comment->status=1;
                 $comment->save();
+                //delete confirmed comment information from Commen_mod table
                 $comment_mod = Comment_mod::where('id',$id)->first();
                 $comment_mod->delete();
             }
         }
-        $comments_mod = Comment_mod::where('status',0)->get();
+        //deleting non-confirmed comments from Comment and Comment_mod table
+        $comments_mod = Comment_mod::all();
         if ( !empty( $comments_mod)  )
             foreach ( $comments_mod  as $comment_mod ) {
                 $comment = Comment::where('id',$comment_mod->id)->first();
