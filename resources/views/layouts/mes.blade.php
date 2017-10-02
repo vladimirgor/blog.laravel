3caee211fff68694d00034a68af064ffdc854062
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-02 21:50:14 +0500

    add left and right sides of  navbar

--- resources/views/layouts/mes.blade.php
+++ resources/views/layouts/mes.blade.php
@@ -49,12 +49,52 @@
                 Galina & Vladimir Goryainov's Blog
             </a>
         </div>
-        <!-- Right Side Of Navbar -->
-        <ul class="nav navbar-nav navbar-right">
-            <!-- Authentication Links -->
-                <li><a href="{{ url('/register') }}">Register</a></li>
-                <li><a href="{{ url('/login') }}">Login</a></li>
-        </ul>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                @can('admin')
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" id="admin"
+                       aria-expanded="false">
+                        Admin<span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="admin">
+                        <li><a href="{{ route('admin') }}">Articles</a></li>
+                        <li><a href="{{ route('admin/user') }}">Users</a></li>
+                    </ul>
+                </li>
+                @endcan
+                @can('moder')
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" id="moder"
+                       aria-expanded="false">
+                        Moderator<span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="moder">
+                        <li><a href="{{ route('moderation') }}">Comments Moderation</a></li>
+                    </ul>
+                </li>
+                @endcan
+            </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            User: {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
     </div>
 </nav>
 
