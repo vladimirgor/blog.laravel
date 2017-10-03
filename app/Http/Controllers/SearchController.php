3c5ddfddc0d620ea539c1f2344c696c73fbe9d7e
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-03 16:38:08 +0500

    function search coded

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Article;
+use App\User;
 
 class SearchController extends Controller
 {
@@ -12,13 +13,42 @@ class SearchController extends Controller
 
         return view('searchForm');
     }
-    public function searchShow(Request $request){
+    public function searchDetails(Request $request){
 
         $this->validate($request,[
             'searchText' => 'required',
         ]);
-        //print_r($_POST);
-        return view('searchShow')->with(['request'=>$request]);
+        return redirect(url('search' .'/'. $request->field .'/' . $request->searchText));
+    }
+    public function search($field,$searchText){
 
+        $articles = Article::select(['id','title','content','comment',
+            'view','image_path','date'])->
+        where($field,'LIKE','%'.$searchText.'%')->
+        orderBy('id','desc')->paginate(PER_PAGE);
+        return view('articlesFound')->with(['articles'=>$articles,
+            'field'=>$field,'searchText'=>$searchText
+        ]);
+    }
+    public function show($id, $field, $searchText, $page, $step=1){
+        $article = Article::select(['id','title','content','view','image_path'])->where('id',$id)->first();
+
+        $comments = Article::find($id)->comments->where('status',1);
+        if ( $step == 1 ){
+            $article->view++;
+            $article->save();
+        }
+        foreach ($comments as $key => $comment ){
+            $user = User::find($comment->user_id);
+            $comment->user_id = $user->name;// change user_id by name
+        }
+        return view('article-contentFound')->with([
+            'comments'=>$comments,
+            'page' => $page,
+            'field' => $field,
+            'searchText' => $searchText,
+            'article'=>$article,
+            'step'=>$step
+        ]);
     }
 }
