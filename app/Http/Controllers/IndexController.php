26e146b38bcf08857a2765624938c90b23d39483
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-03 10:07:42 +0500

    unnecessary lines deleted

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,11 +12,7 @@ class IndexController extends Controller
     protected $header;
 
     public function __construct(){
-        $this->header = 'Hello, World!';
-        $this->message = 'This is a template for a simple marketing or
-        informational website. It includes a large callout called
-        a jumbotron and three supporting pieces of content. Use it
-        as a starting point to create something more unique.';
+
     }
     public function index(){
 
