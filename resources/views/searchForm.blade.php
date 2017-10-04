882e44663c841a910bca4cc2f247b10cbacb745a
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-04 23:48:34 +0500

    changed to laravel view example

--- resources/views/searchForm.blade.php
+++ resources/views/searchForm.blade.php
@@ -2,37 +2,59 @@
 
 @section('content')
     <div class="container">
-        <h2>Search details&#128269</h2>
-        <div class="form">
-            <!-- New Comment Form -->
-            <form method="POST" action="{{route('searchDetails')}}">
-                <h4>Please, choose field to search in.</h4>
-                <div class="radio">
-                    <label>
-                        <input type="radio" name="field"  value="title" checked>
-                        Title
-                    </label>
-                </div>
-                <div class="radio">
-                    <label>
-                        <input type="radio" name="field"  value="content">
-                        Content
-                    </label>
-                </div>
-                <div class="form-group">
-                    <h4>Please, input detail to find.</h4>
-                    <input type="text"  class="form-control" id="SearchText" name="searchText">
-                </div>
-                <!-- Display Validation Errors -->
-                @include('common.errors')
-                <!-- Add Comment Button -->
-                <button type="submit" class="button btn-primary">Submit</button>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Search details&#128269</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('searchDetails') }}">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="field" value="title" checked> Title
+                                        </label>
+                                    </div>
+                                </div>
+                            </div><div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="radio">
+                                        <label>
+                                            <input type="radio" name="field" value="content"> Content
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
 
-                {{ csrf_field() }}
+                            <div class="form-group{{ $errors->has('searchText') ? ' has-error' : '' }}">
+                                <label for="search" class="col-md-4 control-label">Search text</label>
 
-            </form>
+                                <div class="col-md-6">
+                                    <input autofocus id="searh" type="text" class="form-control" name="searchText" value="{{ old('searchText') }}">
 
+                                    @if ($errors->has('searchText'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('searchText') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="button btn-primary">
+                                        <i class="fa fa-btn fa-sign-in"></i> Submit
+                                    </button>
+                                </div>
+                            </div>
+                            @if(Session::has('message'))
+                                <strong>{!!Session::get('message')!!}</strong>
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <hr>
-    </div> <!-- /container -->
+    </div>
 @endsection
