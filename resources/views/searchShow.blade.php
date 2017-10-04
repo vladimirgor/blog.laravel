f13580840f0358c94d6ca7eb9ee2082f210a5d39
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-04 22:28:03 +0500

    unnecessary element

--- resources/views/searchShow.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h2>SearchShow</h2>
-        <p>{{$request->field}}</p>
-        <p>{{$request->searchText}}</p>
-    </div> <!-- /container -->
-@endsection
