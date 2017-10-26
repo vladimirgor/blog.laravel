e59aa8d50146900b2cc6990a9e67634496291101
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-26 16:52:24 +0500

    content for comment_message and comment_error changed

--- resources/views/comment/add-comment.blade.php
+++ resources/views/comment/add-comment.blade.php
@@ -14,18 +14,21 @@
             @include('common.errors')
             <!-- New Comment Form -->
             <form id = "comment_form" method="POST" >
-                <div id="comment_message">
-                    Your comment has been sent!
-                </div>
-                <div id="comment_error">
-                    An error occurred while sending the comment.
-                </div>
                 <div class="form-group">
                     <label for="comment">Comment</label>
-                    <input type="text" autofocus class="form-control" id="comment" name="comment" placeholder="Comment">
+                    <input type="text" required autofocus class="form-control" id="comment"
+                           name="comment" placeholder="Input your comment here, please.">
                 </div>
                 <input type="number" hidden name="article_id" value="{{$article_id}}">
                 <input type="number" hidden name="user_id" value="{{$user_id}}">
+                <div id="comment_message" class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Thanks a lot for the comment!</strong> To be published your comment must pass the moderation procedure.
+                </div>
+                <div id="comment_error" class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>An error occurred while sending the comment.</strong> Apply to the administrator, please.
+                </div>
                 <!-- Add Comment Button -->
                 <button  type="submit" class="button btn-primary">Submit</button>
                 {{ csrf_field() }}
