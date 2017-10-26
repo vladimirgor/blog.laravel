180296dd701d3bdabb51a3e5b65d7a8d035c9b04
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-26 10:39:12 +0500

    empty lines deleted

--- resources/views/comment/add-comment.blade.php
+++ resources/views/comment/add-comment.blade.php
@@ -9,25 +9,20 @@
                 {{$title}}
             </div>
         </div>
-
-            <div class="form">
-                <!-- Display Validation Errors -->
-                @include('common.errors')
-                <!-- New Comment Form -->
-                <form  method="POST" action="{{route('commentStore',['id'=>$article_id, 'page' => $page])}}">
-                    <div class="form-group">
-                        <label for="comment">Comment</label>
-                        <input type="text" autofocus class="form-control" id="comment" name="comment" placeholder="Comment">
-                    </div>
-
-                    <!-- Add Comment Button -->
-                    <button  type="submit" class="button btn-primary">Submit</button>
-
-                    {{ csrf_field() }}
-
-                </form>
-
-            </div>
+        <div class="form">
+            <!-- Display Validation Errors -->
+            @include('common.errors')
+            <!-- New Comment Form -->
+            <form  method="POST" action="{{route('commentStore',['id'=>$article_id, 'page' => $page])}}">
+                <div class="form-group">
+                    <label for="comment">Comment</label>
+                    <input type="text" autofocus class="form-control" id="comment" name="comment" placeholder="Comment">
+                </div>
+                <!-- Add Comment Button -->
+                <button  type="submit" class="button btn-primary">Submit</button>
+                {{ csrf_field() }}
+            </form>
+        </div>
         <hr>
     </div> <!-- /container -->
 @endsection
