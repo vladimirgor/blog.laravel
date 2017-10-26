243d12e372be25643b0c6dc2770499391bb5ca6f
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-26 18:01:40 +0500

    env() function used to get config data

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,45 +27,45 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies($gate);
 
         $gate->define('addNewArticle',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('addImage',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('updateArticle',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('deleteArticle',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('deleteComment',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('deleteUser',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('admin',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('user',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
 
         $gate->define('article',function ($user){
-            return $user->login == AUTH_USER;
+            return $user->login == env('AUTH_USER');
         });
         $gate->define('moder',function ($user){
-            return $user->login == AUTH_MODER;
+            return $user->login == env('AUTH_MODER');
         });
         $gate->define('moderationComments',function ($user){
-            return $user->login == AUTH_MODER;
+            return $user->login == env('AUTH_MODER');
         });
     }
 
