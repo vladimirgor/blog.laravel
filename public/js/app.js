672eef43a805b7dad40d758fc3b129d760aae68a
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-26 11:14:42 +0500

    app.ls coded

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
 /**
  * Created by Владимир on 26.10.2017.
  */
+$(document).ready(function(){
+    $('#comment_form').on('submit', function(e){
+        e.preventDefault();
+
+        $.ajax({
+            type: 'POST',
+            url: '/ajax-comment',
+            data: $('#comment_form').serialize(),
+            success: function(result){
+                console.log(result);
+            }
+        });
+    });
+});
