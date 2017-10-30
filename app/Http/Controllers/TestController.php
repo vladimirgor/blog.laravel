0b7c1aea862fe2ab7823390ebbc2e6c4459c7f62
vladimirgor <vladimirgor2011@yandex.ru>
2017-10-30 19:24:43 +0500

    Test controller changed

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,24 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Pagination\LengthAwarePaginator;
 use App\Http\Requests;
-use App\Article;
+use App\Role;
 
 class TestController extends Controller
 {
-    public function index(){
-       // dump($page);
+    public function index($role=4,$priv=2){
+        $items = Role::where('id_role','=',$role)->get();
+        //$items = ['first','second','third'];
+        return view('test')->with(['items'=>$items]);
 
-        $articles = Article::select(['id','title','content','comment',
-            'view','image_path','date'])->orderBy('id','desc')->paginate(env('PER_PAGE'));
-        //get();
-        //dump($articles);
-       // $articles = new LengthAwarePaginator($collection, $collection->count(), env('PER_PAGE'));
-        //$articles->resolveCurrentPage($page);
-        //dump($articles->links());
-        //$links = str_replace('?page=', '/page/',$articles->links());
-        //dump($links);
-
-        return view('article_test')->with(['articles'=>$articles]);
-       // return('TestController');
     }
 }
